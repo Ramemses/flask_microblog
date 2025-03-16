7ab2cf48cfc153ca2a0dba48f57969f83802ac3e
pussy <pussy.com>
2025-03-16 10:49:31 +0300

    Добавлены итоги 3 урока из mega-book: 	- создана форма и обработка get-запроса при переходе на нее 	- добавлен конфигурационный ключ 	- расширен базовый шаблон 'base.html'

--- my_app/routes.py
+++ my_app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash, request
 from my_app import app
+from my_app.forms import LoginForm
 
 
 
@@ -20,3 +21,20 @@ def index():
 
 
 	return render_template('index.html', title="Главная", user=user, posts=posts)
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+	form = LoginForm()
+	
+	if request.method == 'POST':
+		print(request.form)
+	
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))		
+		redirect( url_for('index') )
+
+
+	return render_template('login.html', title='Sign in', form = form)
+
+	
