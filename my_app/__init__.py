3284c583d241589aa4dc48695ccd5f8f892be922
pussy <pussy.com>
2025-03-18 16:54:10 +0300

    Добавлены итоги 7 главы: 	- обработчики ошибок 404 и 500 	- исправлена ошибка сервера, а именно вылет из порграммы при изменении имени пользователя на существующее 	- добавлено отлавливание ошибок сервера и запись их в файл

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -4,6 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -11,4 +17,16 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from my_app import routes, models
+if not app.debug:
+	if not os.path.exists('logs'):
+		os.mkdir('logs')
+	file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+	file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+	file_handler.setLevel(logging.INFO)
+	app.logger.addHandler(file_handler)
+
+	app.logger.setLevel(logging.INFO)
+	app.logger.info('Microblog startup')
+
+
+from my_app import routes, models, errors
--- my_app/routes.py
+++ my_app/routes.py
@@ -93,7 +93,7 @@ def user(username):
 @app.route('/edit_profile', methods = ['POST', 'GET'])
 @login_required
 def edit_profile():
-	form = EditProfileForm()
+	form = EditProfileForm(current_user.username)
 	if form.validate_on_submit():
 		current_user.username = form.username.data
 		current_user.about_me = form.about_me.data
@@ -106,8 +106,5 @@ def edit_profile():
 		form.username.data = current_user.username
 		form.about_me.data = current_user.about_me
 	return render_template('edit_profile.html', title='Edit profile', form=form)
-
-
-
 
 
